3fb594344ec6872a40b6fff5e3bb42655251c832
Edu Souza <eduardomail@gmail.com>
2017-12-20 18:01:24 +0000

    adjusting controllers

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::SessionsController < ApplicationController
+class Api::V1::SessionsController < Api::V1::BaseController
   respond_to :json
   def create
     user =  User.find_by(email: session_params[:email])
